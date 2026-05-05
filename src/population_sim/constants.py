03ac8cc982cfe0cc711f7a55da2f038a221b7b54
mharrymh <149264867+mharrymh@users.noreply.github.com>
2026-05-05 19:20:41 -0400

    chore(population_sim): define base constants

---
+++ src/population_sim/constants.py
@@ -0,0 +1,23 @@
+DEATH = "death"
+DEATH_RISK_UPDATE = "death_risk_update"
+
+PARTNER_ELIGIBILITY = "partner_eligibility"
+PARTNER_SEARCH = "partner_search"
+PARTNER_SEARCH_RISK_UPDATE = "partner_search_risk_update"
+
+BREAKUP = "breakup"
+END_LONELINESS = "end_loneliness"
+
+PREGNANCY = "pregnancy"
+PREGNANCY_RISK_UPDATE = "pregnancy_risk_update"
+BIRTH = "birth"
+
+COLLECT_STATISTICS = "collect_statistics"
+
+
+PRIORITY_BIRTH = 10
+PRIORITY_DEATH = 20
+PRIORITY_BREAKUP = 30
+PRIORITY_PARTNER = 40
+PRIORITY_PREGNANCY = 50
+PRIORITY_STATISTICS = 99
